fef2df9d50f3b39cb01bc9f392d8c79f86c050ad
TatyanaGalushka <tatyana.primenko@gmail.com>
2023-01-15 14:26:47 +0700

    Скорректированы файлы SQL

--- SQL/SQL_Dz_1.sql
+++ SQL/SQL_Dz_1.sql
@@ -1,28 +1,28 @@
-1.Выведите значение колонки ContactName
+--1. Выведите значение колонки ContactName
 SELECT ContactName
 FROM otusQA
 
-2.Выведите все возможные значения колонки Country
+--2. Выведите все возможные значения колонки Country
 SELECT Country
 FROM otusQA;
 
-3.Выведите все записи, где City имеет значение London
+--3. Выведите все записи, где City имеет значение London
 SELECT *
 FROM otusQA
 WHERE City = 'London'
 
-4.Выведите все записи, где City не равен London
+--4. Выведите все записи, где City не равен London
 SELECT *
 FROM otusQA
 WHERE City != 'London'
 
-5.Выберите все записи, где Country равна Mexico и Postal Code 05021
+--5. Выберите все записи, где Country равна Mexico и Postal Code 05021
 SELECT *
 FROM otusQA
 WHERE Country = 'Mexico'
 AND PostalCode = '05021'
 
-6.Выберете все записи у которых Country равна Mexico или Sweden
+--6. Выберете все записи у которых Country равна Mexico или Sweden
 SELECT *
 FROM otusQA
 WHERE Country = 'Mexico'
